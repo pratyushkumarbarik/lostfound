92ce9aa8a3b034750fe8eb686ff496d80aaf4781
pratyushkumarbarik <pratyushkumar480@gmail.com>
2025-09-03 19:38:23 +0530

    Your

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000'; // Backend URL and port
+const API_BASE_URL = 'https://lostfound-nine.vercel.app'; // Backend URL and port
 
 const api = axios.create({
   baseURL: API_BASE_URL,
